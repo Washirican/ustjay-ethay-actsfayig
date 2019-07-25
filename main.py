f9dfcd0dc2dc1d518642eaa846c12f9c435e4b48
Washirican <danrod@uw.edu>
2019-07-24 22:13:31 -0700

    Pig Latin assignment final commit.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -13,16 +13,35 @@ def get_fact():
 
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
+    fact = facts[0].getText()
 
-    return facts[0].getText()
+    return fact.strip().rstrip('.')
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    page = """
+    <html>
+    <head>
+    <title> Pig Facts </title>
+    <body>
+    <h1>That'll do pig. That'll do.</h1>
+    <p><b>Unnecessary Knowledge:</b>{}</p>
+    <p><b>Piglatinized URL: </b>{} [<a href='{}' target="_blank">Link</a>]</p>
+    </body>
+    </html>
+    """
+    fact = get_fact()
+    response = requests.post(
+        'https://hidden-journey-62459.herokuapp.com/piglatinize/',
+        data={'input_text': fact}
+        )
+    return page.format(
+        fact,
+        response.text.split('\n')[-4].strip(),
+        response.url)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
